5898985385a88ed68f14dab33c99767d317bb9ab
Adesh Gupta <adesh02092000@gmail.com>
2022-06-20 01:11:43 +0530

    first sample route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('first')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT
+
+const app = express()
+
+app.get('/', (req, res) => {
+  //   res.send('Hello!')
+  res.json({ msg: 'Hello, World!' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is spining at PORT : ${PORT}`)
+})
