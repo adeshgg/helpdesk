b378b525dd1df377e34031d903b2d4790e1d531b
Adesh Gupta <adesh02092000@gmail.com>
2022-06-22 00:50:50 +0530

    Added Login and Home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,19 @@
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <section className='heading'>
+        <h1>How can we help you</h1>
+      </section>
+
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View your Tickets
+      </Link>
+    </>
+  )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,63 @@
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
 export default function Login() {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = e => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('submitted login')
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              placeholder='Enter your password'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
