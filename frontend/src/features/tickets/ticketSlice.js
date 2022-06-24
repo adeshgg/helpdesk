2b795105a1e25c944e97733ca757d885c135d900
Adesh Gupta <adesh02092000@gmail.com>
2022-06-24 20:55:36 +0530

    Logic for create ticket in slice and service files

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-// import ticketService from './ticketService'
+import ticketService from './ticketService'
 
 const initialState = {
   tickets: [],
@@ -9,6 +9,27 @@ const initialState = {
   isLoading: false,
   message: '',
 }
+
+// action function
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      // to grab the user token (since we need to send it, as this is a protected route)
+      // Use the getState() on the thunkAPI object
+      const token = thunkAPI.getState().auth.user.token
+      return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
 
 export const ticketSlice = createSlice({
   name: 'ticket',
