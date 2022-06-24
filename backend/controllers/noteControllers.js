5b0ba7bf9cdac10a1c95293ca537729698de4496
Adesh Gupta <adesh02092000@gmail.com>
2022-06-25 02:45:16 +0530

    Added createNote endpoint, backend done :medal_sports:

--- backend/controllers/noteControllers.js
+++ backend/controllers/noteControllers.js
@@ -23,6 +23,31 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes)
 })
 
+// @desc Create note for a ticket
+// @route /api/tickets/:ticketId/notes
+// @access Private
+const addNote = asyncHandler(async (req, res) => {
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not Found')
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId)
+  if (ticket.user._id.toString() !== req.user._id.toString()) {
+    res.status(401)
+    throw new Error('Request not authorized')
+  }
+
+  const note = await Note.create({
+    user: req.user._id,
+    ticket: req.params.ticketId,
+    text: req.body.text,
+  })
+
+  res.status(201).json(note)
+})
+
 module.exports = {
   getNotes,
+  addNote,
 }
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 // endPoint : api/tickets/:ticketId/notes
 const router = express.Router({ mergeParams: true })
 
-const { getNotes } = require('../controllers/noteControllers')
+const { getNotes, addNote } = require('../controllers/noteControllers')
 const protectRoutes = require('../middleware/authMiddleware')
 
 router.get('/', protectRoutes, getNotes)
+router.post('/', protectRoutes, addNote)
 
 module.exports = router
