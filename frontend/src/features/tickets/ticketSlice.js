8f68ded1fca07c77672368b8c4aaa67840d9a458
Adesh Gupta <adesh02092000@gmail.com>
2022-06-25 00:33:36 +0530

    Added extra reducers

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -72,6 +72,19 @@ export const ticketSlice = createSlice({
         state.isError = true
         state.message = action.payload
       })
+      .addCase(getTickets.pending, state => {
+        state.isLoading = true
+      })
+      .addCase(getTickets.fulfilled, (state, action) => {
+        state.isSuccessful = true
+        state.isLoading = false
+        state.tickets = action.payload
+      })
+      .addCase(getTickets.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   },
 })
 
