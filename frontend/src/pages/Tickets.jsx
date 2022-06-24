87602f67ab11c664624fc1fb400c0fce4fe02370
Adesh Gupta <adesh02092000@gmail.com>
2022-06-25 01:03:25 +0530

    UI for Tickets component, GetTickets functionality done :medal_sports:

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
+import TicketItem from '../components/TicketItem'
 
 export default function Tickets() {
   const { tickets, isLoading, isError, isSuccessful } = useSelector(
@@ -25,5 +26,21 @@ export default function Tickets() {
     return <Spinner />
   }
 
-  return <div>Tickets</div>
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map(ticket => {
+          return <TicketItem key={ticket._id} ticket={ticket} />
+        })}
+      </div>
+    </>
+  )
 }
