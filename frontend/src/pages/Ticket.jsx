c952f7518a57b3fd82acc88fa4aea1501e87742f
Adesh Gupta <adesh02092000@gmail.com>
2022-06-25 01:33:36 +0530

    Added basic layout for Ticket component

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { getTicket } from '../features/tickets/ticketSlice'
+import Spinner from '../components/Spinner'
+import BackButton from '../components/BackButton'
 
 export default function Ticket() {
   const { ticket, isLoading, isSuccessful, isError, message } = useSelector(
@@ -20,5 +22,29 @@ export default function Ticket() {
     dispatch(getTicket(ticketId))
   }, [isError, dispatch, ticketId])
 
-  return <div>Ticket</div>
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton url='/tickets' />
+        <h2>
+          Ticket ID : {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted : {new Date(ticket.createdAt).toLocaleString('en-IN')}
+        </h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Description of the issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  )
 }
