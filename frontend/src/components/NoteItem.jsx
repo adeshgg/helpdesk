ffccc53df1b7f88f5bdd64beddac7fae84e86c42
Adesh Gupta <adesh02092000@gmail.com>
2022-06-25 19:57:58 +0530

    Fetch the notes owner for UI updates

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,18 +1,39 @@
 import { useSelector } from 'react-redux'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const API_URL = 'http://localhost:5000/api/users/'
 
 export default function NoteItem({ note }) {
   const { user } = useSelector(state => state.auth)
+  const [noteUser, setNoteUser] = useState({})
+
+  const getUser = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    }
+
+    const response = await axios.get(API_URL + `${note.user}`, config)
+    setNoteUser(response.data)
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
 
   return (
     <div
       className='note'
       style={{
-        backgroundColor: user.isAdmin === true ? 'rgba(0,0,0,0.7)' : '#fff',
-        color: user.isAdmin === true ? '#fff' : '#000',
+        backgroundColor: noteUser.isAdmin ? 'rgba(0,0,0,0.7)' : '#fff',
+        color: noteUser.isAdmin ? '#fff' : '#000',
       }}
     >
       <h4>
-        Note from {user.isAdmin ? <span>Admin</span> : <span>{user.name}</span>}
+        Note from{' '}
+        {noteUser.isAdmin ? <span>Admin</span> : <span>{noteUser.name}</span>}
       </h4>
       <p>{note.text}</p>
       <div className='note-date'>
